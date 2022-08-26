1b9ac810e8b1660d6332de1778b008f86aeeb361
shirobutton <shirobutton@gmail.com>
2022-08-26 19:45:10 +0900

    Fragmentを再度表示した際に更新されるように変更

--- app/src/main/java/com/example/trypaging3/ListFragment.kt
+++ app/src/main/java/com/example/trypaging3/ListFragment.kt
@@ -14,16 +14,23 @@ import kotlinx.coroutines.flow.onEach
 class ListFragment : Fragment(R.layout.fragment_list) {
 
     private val viewModel: ListViewModel by viewModels()
+    private var adapter: CardListAdapter? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentListBinding.bind(view)
-        val adapter = CardListAdapter()
+        adapter = CardListAdapter()
         binding.recyclerView.adapter = adapter
         binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
         viewModel.listData
             .conflate()
-            .onEach { adapter.submitData(it) }
+            .onEach { adapter?.submitData(it) }
             .launchIn(viewLifecycleOwner.lifecycleScope)
+    }
+
+    override fun onDestroyView() {
+        adapter?.refresh()
+        adapter = null
+        super.onDestroyView()
     }
 }
